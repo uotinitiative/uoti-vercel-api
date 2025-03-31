de952321b4b93ebaaedb5391a4344f78de069ef4
uotinitiative <vatsal@readvatsal.com>
2025-03-31 19:51:58 +0530

    Update download.js

--- api/download.js
+++ api/download.js
@@ -3,6 +3,10 @@ import { readFileSync } from "fs";
 import { join } from "path";
 
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "https://uotinitiative.org");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   const { file } = req.query;
 
   if (!file) {
@@ -14,8 +18,8 @@ export default async function handler(req, res) {
     const privateKey = readFileSync(privateKeyPath, "utf8");
 
     const signedUrl = getSignedUrl({
-      url: `https://df3f5t1ky9d5t.cloudfront.net/${file}`, // <== change this later
-      keyPairId: "K3DIU5P2B5DBYE", // <== change this later
+      url: `https://df3f5t1ky9d5t.cloudfront.net/${file}`,
+      keyPairId: "K3DIU5P2B5DBYE",
       dateLessThan: new Date(Date.now() + 5 * 60 * 1000),
       privateKey,
     });
